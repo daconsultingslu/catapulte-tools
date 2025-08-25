ca4b0af7dcfb6fb3e40755751765592d4e8bbae6
Damien Aulombard <damien@daconsulting.me>
2025-08-25 21:17:21 +0200

    fix: allow OPTIONS on rest api

--- src/Controller/Tools/Trial/TrialToolRestController.php
+++ src/Controller/Tools/Trial/TrialToolRestController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TrialToolRestController extends AbstractController
 {
-    #[Route('/rest/trial/new', name: 'trial_tool_api', methods: ['POST'])]
+    #[Route('/rest/trial/new', name: 'trial_tool_api', methods: ['POST', 'OPTIONS'])]
     public function post(Request $request, LoggerInterface $logger, EntityManagerInterface $em): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
