56035eb09ba3351ee2fb7a7b59f8577e528d64ec
Damien Aulombard <damien@daconsulting.me>
2025-11-05 08:44:51 +0100

    feat: change catapulte to stellantis

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -35,7 +35,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Catapulte')
+            ->setTitle('Stellantis')
             ->setTranslationDomain('admin');
     }
 
